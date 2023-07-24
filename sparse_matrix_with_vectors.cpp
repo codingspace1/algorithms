e0506131779a20c27c704aa3f0a4394ea68799d1
Vishal Sharma <hypersonicindian@gmail.com>
2023-07-24 11:37:23 +0530

    updated docs

--- sparse_matrix_with_vectors.cpp
+++ sparse_matrix_with_vectors.cpp
@@ -1,17 +1,34 @@
-/*
-This program checks if a given matrix is sparse or not
-i.e in a sparse matrix zero valued elements are much more as compared to no zero values elements
-here if zero valued elemts are more than the 50% size of the matrix, we will call it a sparse matrix
-*/
+/*********************************************************************************
+* @file
+* @brief [Sparse Matrix]
+* @details
+* A sparse matrix is a special type of matrix that contains mostly zeros. 
+* In a regular matrix, most of the elements have values, but in a sparse matrix, the majority of the elements are zero. 
+* This means that only a few elements have non-zero values, making the matrix "sparse" with empty spaces.
+* Imagine a grid-like structure where each cell holds a number. 
+* In a regular matrix, most cells have numbers in them, but in a sparse matrix, many cells are empty (zeros). 
+* These empty cells make the sparse matrix more efficient to store and work with because you don't need to remember or process as many values.
+* Sparse matrices are common in various fields like scientific computing, graph theory, and data analysis when dealing with large datasets or structures where only a small fraction of elements have meaningful values. Using sparse matrices can save memory and computational resources, which is beneficial in many practical applications.
+*
+* ### Implementation
+
+* We counts the number of zero valued elements in the Matrix
+* Check if the count is more than the half of the size of matrix
+* If 50% of the elements from the matrix and zero valued, we call it a sparse matrix.
+
+* @author [Vishal Sharma] (https://github.com/codingspace1)
+***************************************************************************************/
 #include<iostream> ///for I/O operations
 #include<vector>   /// for std::vector
 #include<cassert> /// for assert
 
-/*
-counts the number of  zero valued elements
-check if the count is more than the half of the size of matrix
 
-*/
+/***
+ * @brief 
+ * function to check if matrix is sparse or not
+ * @param vect Matrix to be tested
+ * @returns zero if matrix is not sparse
+***/
 bool is_sparse_matrix(std::vector<std::vector<int>> vect)
 {
     int zero_counter = 0;
@@ -27,9 +44,16 @@ bool is_sparse_matrix(std::vector<std::vector<int>> vect)
         }
     }
 
-    return (zero_counter>(vect.size() * vect[0].size())* 0.5);
+    return (zero_counter>(vect.size() * vect[0].size())* 0.5); //return 1 if zero values are more than non zero values
 
 }
+
+/*******************************************************************************
+ * @brief 
+ * function to display the Matrix elements
+ * @param vect Matrix to be displayed
+ * @returns void
+*******************************************************************************/
 
 void display(std::vector<std::vector<int>> vect)
 {
@@ -91,13 +115,13 @@ void test3()
 
 }
 
-int main()
-{
-    /*******************************************************************************
+ /*******************************************************************************
      * @brief Main function
      * @returns 0 on exit
      *******************************************************************************/
 
+int main()
+{
     test1();  // run self-test implementation #1
     test2();  // run self-test implementation #2
     test3();  // run self-test implementation #3
